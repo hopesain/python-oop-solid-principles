05f472f1d04fbfaacda05b51d813c51dc2f54abe
hopesain <hopessain@gmail.com>
2025-07-31 11:09:03 +0100

    Completed the static attributes.

---
+++ 01_object_oriented_programming/04_static_attributes.py
@@ -0,0 +1,20 @@
+# Static Attributes
+# Attributes that can belong to a class but not to an instance.
+# Useful for things like count etc.
+
+
+class Post:
+    count_posts = 0
+
+    def __init__(self, author: str, content: str):
+        self.author = author
+        self.content = content
+        Post.count_posts += 1
+
+    def display_posts(self):
+        return f"Author: '{self.author}', Content: '{self.content}'"
+    
+first_post = Post("HopeSain", "failedState")
+second_post = Post("MirrorCanesat", "hopeLost")
+
+print(Post.count_posts)
